42e37839f61390f783aafe41c7101a7c9224bcab
root <root@test.tickbook.net>
2014-03-20 14:11:20 -0500

    blackhole-client: removing routes and views as this is a console type application for now.

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var crypto = require('crypto');
@@ -19,7 +17,7 @@ var config = require('./config');
 app.set('port', process.env.PORT || 3000);
 
 http.createServer(app).listen(app.get('port'), function(){
-    console.log('blackhole server listening on port ' + app.get('port'));
+    console.log('blackhole client started ...');
 
     var socket = clientio.connect(config.blackhole.host , { port: config.blackhole.port });
     var cb_client = new Crossbar({
